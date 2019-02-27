fcc5cb3c019d225a26b9fb00dad822d7358884fa
Gordon Bai <gdbaifdu@gmail.com>
2019-02-27 10:39:07 +0000

    Create gammu-receive.sh

---
+++ gammu-receive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TOKEN="YOUR TELEGRAM BOT TOKEN"
+CHAT_ID=YOUR CHAT ID
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+NL=$'\n'
+declare -i i MSGS
+MSGS=${SMS_MESSAGES}
+CONTENT="${SMS_1_NUMBER}${NL}"
+i=1
+while [ $i -le $MSGS ]; do
+    declare "PART"="SMS_${i}_TEXT"
+    CONTENT="${CONTENT}${!PART}"
+    i=$(($i+1))
+done
+eval "curl -s -X POST $URL -d chat_id=$CHAT_ID -d text=\"${CONTENT}\""
