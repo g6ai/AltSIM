97effe0551fbb2d6824a02f4e02e07c785c5d3c1
GdBai <gdbaifdu@gmail.com>
2019-04-22 19:08:29 +0100

    Parse '&', '<' and '>' according to Telegram bots usage.

--- gammu-receive.sh
+++ gammu-receive.sh
@@ -12,4 +12,7 @@ while [ $i -le $MSGS ]; do
     CONTENT="${CONTENT}${!PART}"
     i=$(($i+1))
 done
+CONTENT=${CONTENT//&/%26}
+CONTENT=${CONTENT//</%3C}
+CONTENT=${CONTENT//>/%3E}
 eval "curl -s -X POST $URL -d chat_id=$CHAT_ID -d text=\"${CONTENT}\""
